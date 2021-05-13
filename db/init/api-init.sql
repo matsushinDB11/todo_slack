6a7770657c31cb1cd443d51441082e520133b333
Shintaro Matsumoto <matsushin35@gmail.com>
2021-05-14 02:10:34 +0900

    fix init.sql

--- db/init/api-init.sql
+++ db/init/api-init.sql
@@ -2,21 +2,21 @@ SET CHARSET UTF8;
 -- ユーザー情報
 CREATE TABLE users (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_id VARCHAR(30) NOT NULL UNIQUE,
+  user_name VARCHAR(30) NOT NULL UNIQUE,
   password VARCHAR(20) NOT NULL,
   slack_id VARCHAR(30) DEFAULT NULL,
-  PRIMARY KEY (id, user_id)
+  PRIMARY KEY (id, user_name)
 );
 
 -- テスト用サンプルユーザー
-INSERT INTO users (user_id, password) VALUES
+INSERT INTO users (user_name, password) VALUES
     ('matsumoto01', 'abcd'),('kasukabe01', 'efgh'),
     ('sakaguchi01', 'ijkl'), ('kawase01', 'mnop');
 
 -- タスク本体
 CREATE TABLE contents (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT, -- 管理用id
-    user_id INT UNSIGNED NOT NULL, -- タスクを入力したユーザーのid -- usersテーブルのid
+    user_name INT UNSIGNED NOT NULL, -- タスクを入力したユーザーのid -- usersテーブルのid
     text TEXT NOT NULL, -- タスク名
     task_limit DATETIME DEFAULT NULL, -- タスク完了期限
     done TINYINT(1) DEFAULT 0, -- タスクが完了したか？ 0 => false, 0以外(通常1) => true
@@ -24,8 +24,8 @@ CREATE TABLE contents (
 );
 
 -- テスト用サンプルタスク
-INSERT INTO contents (user_id, text) VALUES
-    (1, 'データベース定義の作成'), (2, '認証周りの実装'),
-    (3, 'hoge'), (4, 'hogege'),
-    (1, '初期SQL作成'), (2, '認証周りの検証'),
-    (3, 'fuga'), (4, 'fugaga');
+INSERT INTO contents (user_name, text) VALUES
+    ('matsumoto01', 'データベース定義の作成'), ('kasukabe01', '認証周りの実装'),
+    ('sakaguchi01', 'hoge'), ('kawase01', 'hogege'),
+    ('matsumoto01', '初期SQL作成'), ('kasukabe01', '認証周りの検証'),
+    ('sakaguchi01', 'fuga'), ('kawase01', 'fugaga');
